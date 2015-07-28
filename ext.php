67d8bfd76d8150c06980f85cd67fdb3aaab10628
Jakub Senko <jakubsenko@gmail.com>
2015-07-28 18:17:24 +0200

    Add basic files

---
+++ ext.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* User Statistics extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Jakub Senko <jakubsenko@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace senky\userstatistics;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
