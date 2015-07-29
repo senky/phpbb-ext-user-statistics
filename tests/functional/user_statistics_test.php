a5e017477a991a32e1b1ea15930b27c6278c4d89
Jakub Senko <jakubsenko@gmail.com>
2015-07-29 12:06:10 +0200

    Add functional test

---
+++ tests/functional/user_statistics_test.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* User Statistics extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Jakub Senko <jakubsenko@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace senky\userstatistics\tests\functional;
+
+$_SERVER['PHPBB_FUNCTIONAL_URL'] = 'http://localhost/phpbb/phpBB/';
+
+/**
+* @group functional
+*/
+class user_statistics_test extends \phpbb_functional_test_case
+{
+	/**
+	* Define the extensions to be tested
+	*
+	* @return array vendor/name of extension(s) to test
+	*/
+	static protected function setup_extensions()
+	{
+		return array('senky/userstatistics');
+	}
+
+	public function setUp()
+	{
+		global $phpbb_functional_url;
+
+		parent::setUp();
+		$this->add_lang_ext('senky/userstatistics', array('user_statistics'));
+	}
+
+	/**
+	* Test loading the user statistics
+	*/
+	public function test_view_user_statistics()
+	{
+		$this->login();
+
+		$crawler = self::request('GET', 'index.php');
+		$this->assertContains('Your IP', $crawler->filter('#user-statistics thead th:first-child')->text());
+	}
+}
